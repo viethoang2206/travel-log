8deb7e45224a062a4fb4eff86427b53f686c4cc3
viethoang2206 <viethoang220600@gmail.com>
2022-06-17 13:34:14 +0700

    make it better

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 app.use(cors());
-app.listen(80, function () {
+app.listen(5001, function () {
   console.log("CORS-enabled web server listening on port 80");
 });
 const PORT = process.env.PORT || 5000;
