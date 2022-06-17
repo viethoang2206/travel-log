ec4f40f0449660058f586500099a16c0516b7210
viethoang2206 <viethoang220600@gmail.com>
2022-06-17 11:37:18 +0700

    make it better

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 const connectDB = async () => {
   await mongoose
     .connect(
-      `mongodb+srv://admin:root@cluster0.tsd2mac.mongodb.net/travels?retryWrites=true&w=majority`
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.tsd2mac.mongodb.net/travels?retryWrites=true&w=majority`
     )
     .then(() => {
       console.log("connected to DB");
